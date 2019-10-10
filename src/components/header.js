56226c48b102cb2b120ca49008b41ee2e93b8582
Brandon Orther <brandon@uptrend.tech>
2019-10-09 21:09:38 -0700

    Update header to show logomark on smaller screens

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import { Header as ThemeHeader, jsx, Flex, Box } from 'theme-ui'
 
 import logo from '../images/uptrend-logo.svg'
+import logoDiamond from '../images/uptrend-logo--diamond.svg'
 import NavLink from '../components/nav-link'
 
 const styles = {
@@ -18,45 +19,56 @@ const styles = {
 const Header = () => {
   return (
     <ThemeHeader>
-      <Flex
+      <Box
         sx={{
           variant: 'styles.contain',
-          justifyContent: 'space-between',
-          my: 24,
         }}
       >
-        <Box>
-          {/*<!-- Removed the title attribute -->*/}
-          {/*<!-- It is an anti-pattern -->*/}
-          <Link to="/#top">
-            <img
-              src={logo}
-              alt="Uptrend Logo"
-              sx={{ height: 48, display: 'block' }}
-            />
-          </Link>
-        </Box>
+        <Flex
+          sx={{
+            justifyContent: ['center', 'space-between'],
+            my: ['8px', '16px', 24],
+          }}
+        >
+          <img
+            src={logoDiamond}
+            alt="Uptrend Logomark"
+            sx={{ height: 48, display: ['block', 'none'] }}
+          />
 
-        {/*<!-- Removed the aria-label -->*/}
-        {/*<!-- Its position implies it is primary -->*/}
-        {/*<!-- Its presence adds noise for SR users -->*/}
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/#top">Home</NavLink>
-            </li>
-            <li>
-              <NavLink href="/#what-we-do">What We Do</NavLink>
-            </li>
-            <li>
-              <NavLink href="/#our-work">Our Work</NavLink>
-            </li>
-            <li>
-              <NavLink href="/#contact-us">Contact Us</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </Flex>
+          <Box sx={{ display: ['none', 'block'] }}>
+            {/*<!-- Removed the title attribute -->*/}
+            {/*<!-- It is an anti-pattern -->*/}
+            <Link to="/#top">
+              <img
+                src={logo}
+                alt="Uptrend Logo"
+                sx={{ height: 48, display: 'block' }}
+              />
+            </Link>
+          </Box>
+
+          {/*<!-- Removed the aria-label -->*/}
+          {/*<!-- Its position implies it is primary -->*/}
+          {/*<!-- Its presence adds noise for SR users -->*/}
+          <nav sx={{ display: ['none', 'none', 'block'] }}>
+            <ul>
+              <li>
+                <NavLink to="/#top">Home</NavLink>
+              </li>
+              <li>
+                <NavLink href="/#what-we-do">What We Do</NavLink>
+              </li>
+              <li>
+                <NavLink href="/#our-work">Our Work</NavLink>
+              </li>
+              <li>
+                <NavLink href="/#contact-us">Contact Us</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </Flex>
+      </Box>
     </ThemeHeader>
   )
 }
