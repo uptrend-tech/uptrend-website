fdb6fcac02e8ea7da3a5868a5e6ad57210b4e426
Brandon Orther <brandon@uptrend.tech>
2019-10-23 17:33:57 -0700

    Add responsive styling to Our Work section

--- src/components/home.page/index.js
+++ src/components/home.page/index.js
@@ -1,12 +1,12 @@
 /** @jsx jsx */
 import React from 'react'
-import { jsx, Styled, Box } from 'theme-ui'
-import Img from 'gatsby-image'
+import { jsx, Box } from 'theme-ui'
 
 import Hero from './hero'
 import ClientShoutout from './client-shoutout'
 import WhatWeDo from './what-we-do'
 import OurProcess from './our-process'
+import OurWork from './our-work'
 
 const RawHtml = ({ data }) => {
   return (
@@ -24,57 +24,8 @@ const RawHtml = ({ data }) => {
         <OurProcess />
       </section>
 
-      {/*<!-- Not sure <section> adds value here -->*/}
-      {/*<!-- Leaving as it does not add SR noise -->*/}
       <section>
-        <Box sx={{ variant: 'styles.lightSection' }}>
-          <Box sx={{ variant: 'styles.ourWork' }}>
-            <Styled.h2>Our Work</Styled.h2>
-            <Styled.ul sx={{ variant: 'styles.ourWorkExamples' }}>
-              <Styled.li>
-                <Img
-                  fluid={data.ourWorkCareCarImage.childImageSharp.fluid}
-                  sx={{ borderRadius: 5 }}
-                />
-                <Styled.h3>CareCar</Styled.h3>
-                <Styled.p sx={{ m: 0, variant: 'text.tightDim' }}>
-                  CareCar provides non-emergency medical transportation services
-                  to health plans and their members using a network of certified
-                  drivers. When getting started they needed a software platform
-                  to facilitate their core business. Uptrend helped build an
-                  initial prototype, which CareCar used to find their first
-                  customers and secure funding.
-                </Styled.p>
-                <Styled.h4>Services Provided:</Styled.h4>
-                <Styled.p sx={{ variant: 'styles.ourWorkServicesProvided' }}>
-                  Product Design | UX Design | UI Design | System Design |
-                  Software Development | Software Support | Infrastructure
-                  Management
-                </Styled.p>
-              </Styled.li>
-              <Styled.li sx={{ '&&': { gridColumnStart: 7 } }}>
-                <Img
-                  fluid={data.ourWorkTunelinksImage.childImageSharp.fluid}
-                  sx={{ borderRadius: 5 }}
-                />
-                <Styled.h3>Tunelinks</Styled.h3>
-                <Styled.p sx={{ m: 0, variant: 'text.tightDim' }}>
-                  Tunelinks is an online distribution platform for streaming and
-                  downloading new Hip-Hop and R&B music. They needed a system
-                  for standardizing music that arrived in a number of different
-                  formats and serving it to thousands of users at once. Uptrend
-                  designed and built this system, allowing Tunelinks to keep
-                  costs low while serving music to over 100k users a month.
-                </Styled.p>
-                <Styled.h4>Services Provided:</Styled.h4>
-                <Styled.p sx={{ variant: 'styles.ourWorkServicesProvided' }}>
-                  System Design | Software Development | Software Support |
-                  Infrastructure Management
-                </Styled.p>
-              </Styled.li>
-            </Styled.ul>
-          </Box>
-        </Box>
+        <OurWork data={data} />
       </section>
 
       <section>
