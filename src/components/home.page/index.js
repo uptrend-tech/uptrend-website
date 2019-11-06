47582f2d0c4445d4ddfe151a15b40558f26a865f
Brandon Orther <brandon@uptrend.tech>
2019-11-06 08:35:51 -0800

    Fix scroll top by setting section overflow=hidden

--- src/components/home.page/index.js
+++ src/components/home.page/index.js
@@ -9,6 +9,10 @@ import OurProcess from './our-process'
 import OurWork from './our-work'
 import ContactUsSection from './contact-us'
 
+const Section = ({ sx = {}, ...props }) => (
+  <section sx={{ ...sx, overflow: 'hidden' }} {...props} />
+)
+
 const RawHtml = ({ data }) => {
   return (
     <>
@@ -17,21 +21,21 @@ const RawHtml = ({ data }) => {
 
       {/*<!-- Not sure <section> adds value here -->*/}
       {/*<!-- Leaving as it does not add SR noise -->*/}
-      <section id="what-we-do">
+      <Section id="what-we-do">
         <WhatWeDo />
-      </section>
+      </Section>
 
-      <section id="our-process">
+      <Section id="our-process">
         <OurProcess />
-      </section>
+      </Section>
 
-      <section id="our-work">
+      <Section id="our-work">
         <OurWork data={data} />
-      </section>
+      </Section>
 
-      <section id="contact-us">
+      <Section id="contact-us">
         <ContactUsSection />
-      </section>
+      </Section>
     </>
   )
 }
