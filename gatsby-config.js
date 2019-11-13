d6e8b835bc726504092632d522f49c69832a974b
Brandon Orther <brandon@uptrend.tech>
2019-11-13 13:08:28 -0800

    Add manifest & offline gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,25 @@ module.exports = {
         // cookieDomain: 'example.com',
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Uptrend Technology',
+        short_name: 'Uptrend',
+        start_url: '/',
+        background_color: '#363648',
+        theme_color: '#706bf6',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    'gatsby-plugin-offline',
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
